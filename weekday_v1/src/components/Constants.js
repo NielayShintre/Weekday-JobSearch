429ba0ed4a7e6db41a4289da8e527df638db7cbf
Nielay Shintre <nielayshintre18@gmail.com>
2024-05-10 12:19:33 +0530

    created a constants file for the filter options

---
+++ weekday_v1/src/components/Constants.js
@@ -0,0 +1,67 @@
+export const roles = [
+    { role: "Full Stack", category: "Engineering" },
+    { role: "Tech Lead", category: "Engineering" },
+    { role: "Data Engineer", category: "Engineering" },
+    { role: "Backend", category: "Engineering" },
+    { role: "Frontend", category: "Engineering" },
+    { role: "IOS", category: "Engineering" },
+    { role: "Flutter", category: "Engineering" },
+    { role: "React Native", category: "Engineering" },
+    { role: "Android", category: "Engineering" },
+    { role: "DevOps", category: "Engineering" },
+    { role: "Data Science", category: "Engineering" },
+    { role: "Deep Learning", category: "Engineering" },
+    { role: "Web3", category: "Engineering" },
+    { role: "Computer Vision", category: "Engineering" },
+    { role: "Test/QA", category: "Engineering" },
+    { role: "SRE", category: "Engineering" },
+    { role: "Data Infrastruture", category: "Engineering" },
+    { role: "Graphic Designer", category: "Design" },
+    { role: "Product Designer", category: "Design" },
+    { role: "Design Manager", category: "Design" },
+    { role: "Designer", category: "Design" },
+    { role: "Product Manager", category: "Product" },
+    { role: "Operations Manager Manager", category: "Operations" },
+    { role: "Founder's Office/Chief of Staff", category: "Operations" },
+    { role: "Sales Developement Representative", category: "Sales" },
+    { role: "Account Executive", category: "Sales" },
+    { role: "Digital Marketing Manager", category: "Marketing" },
+    { role: "Growth Hacker", category: "Marketing" },
+    { role: "Marketing", category: "Marketing" },
+    { role: "Product Marketing Manager", category: "Marketing" },
+    { role: "Hardware", category: "Other Engineering" },
+    { role: "Mechanical", category: "Other Engineering" },
+    { role: "Systems", category: "Other Engineering" },
+    { role: "Business Analyst", category: "Business Analyst" },
+    { role: "Data Analyst", category: "Data Analyst" },
+    { role: "Project Manager", category: "Project Manager" },
+    { role: "Legal", category: "Legal" },
+    { role: "HR", category: "HR" },
+    { role: "Finance", category: "Finance" },
+  ];
+
+  export const techStack = [
+    "Javascript",
+    "React",
+    "Python",
+    "Java",
+    "Ruby/Rails",
+    "Django",
+    "C#",
+    "Kotlin",
+    "GoLang",
+    "GraphQL",
+    "Flask",
+    "Typescript",
+    "AWS",
+    "Rust",
+    "Node",
+  ];
+
+  export const data = ["Remote", "In-Office", "Hybrid"];
+
+  export const numbers = ["11-20", "51-100", "101-200", "201-500", "500+"];
+
+  export const basePay = [0, 10, 20, 30, 40, 50, 60, 70];
+
+  export const experiences = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
